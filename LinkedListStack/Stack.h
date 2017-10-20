1581fd9426f1d92155a5ab5f8dabd084c2862a39
MCanterel <nelsnelsnels@gmail.com>
2017-10-19 22:20:36 -0400

    Retarget and first commit

--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -1,186 +1,63 @@
 #pragma once
+#include <iostream>
 
-class Stack
-{
+class Stack {
 private:
-	class Element
-	{
+	class Element {
 	public:
-		Element( int val,Element* pNext )
-			:
-			val( val ),
-			pNext( pNext )
-		{}
-		Element( const Element& src )
-			:
-			val( src.val )
-		{
-			if( src.pNext != nullptr )
-			{
-				pNext = new Element( *src.pNext );
-			}
-		}
-		Element& operator=( const Element& ) = delete;
-		int GetVal() const
-		{
-			return val;
-		}
-		Element* Disconnect()
-		{
-			auto pTemp = pNext;
-			pNext = nullptr;
-			return pTemp;
-		}
-		int CountElements() const
-		{
-			if( pNext != nullptr )
-			{
-				return pNext->CountElements() + 1;
-			}
-			else
-			{
-				return 1;
-			}
-		}
-		~Element()
-		{
-			delete pNext;
-			pNext = nullptr;
-		}
-	// there is a way to keep these private
-	// by making Iterator a friend of Element
-	// but it is not worth the hassle(hoff)
-	// private:
 		int val;
-		Element* pNext = nullptr;
+		Element *next;
+		Element ( ) : next ( nullptr ) {};
+		Element ( int val ) :val ( val ), next ( nullptr ) {};
+		/*~Element ( ) {
+			delete next;
+			next = nullptr;
+		}*/
 	};
+private:
+	Element *head;
+	Element *tail;
 public:
-	class Iterator
-	{
-	public:
-		Iterator() = default;
-		Iterator( Element* pElement )
-			:
-			pElement( pElement )
-		{}
-		Iterator& operator++()
-		{
-			pElement = pElement->pNext;
-			return *this;
-		}
-		int& operator*() const
-		{
-			return pElement->val;
-		}
-		bool operator!=( Iterator rhs ) const
-		{
-			return pElement != rhs.pElement;
-		}
-	private:
-		Element* pElement = nullptr;
-	};
-	class ConstIterator
-	{
-	public:
-		ConstIterator() = default;
-		ConstIterator( const Element* pElement )
-			:
-			pElement( pElement )
-		{}
-		ConstIterator& operator++()
-		{
-			pElement = pElement->pNext;
-			return *this;
-		}
-		const int& operator*() const
-		{
-			return pElement->val;
-		}
-		bool operator!=( ConstIterator rhs ) const
-		{
-			return pElement != rhs.pElement;
-		}
-	private:
-		const Element* pElement = nullptr;
-	};
-public:
-	Stack() = default;
-	Stack( const Stack& src )
-	{
-		*this = src;
+	Stack ( ) {
+		head = nullptr;
+		tail = nullptr;
 	}
+	Stack ( const Stack& source ) {
+		head = source.head;
+		tail = source.tail;
+	}
+
 	Stack& operator=( const Stack& src )
 	{
-		if( &src != this )
+		if ( !Empty ( ) )
 		{
-			if( !Empty() )
-			{
-				delete pTop;
-				pTop = nullptr;
-			}
+			delete pTop;
+			pTop = nullptr;
+		}
 
-			if( !src.Empty() )
-			{
-				pTop = new Element( *src.pTop );
-			}
+		if ( !src.Empty ( ) )
+		{
+			pTop = new Element ( *src.pTop );
 		}
 		return *this;
 	}
-	~Stack()
-	{
-		delete pTop;
-		pTop = nullptr;
+
+
+
+	/*Stack& operator=( const Stack& source ) {
+
+	}*/
+
+	~Stack ( ) {
+		//delete head;
+		//delete tail;
+		//head = nullptr;
+		//tail = nullptr;
 	}
-	void Push( int val )
-	{
-		pTop = new Element( val,pTop );
-	}
-	int Pop()
-	{
-		if( !Empty() )
-		{
-			const int tempVal = pTop->GetVal();
-			auto pOldTop = pTop;
-			pTop = pTop->Disconnect();
-			delete pOldTop;
-			return tempVal;
-		}
-		else
-		{
-			return -1;
-		}
-	}
-	int Size() const
-	{
-		if( !Empty() )
-		{
-			return pTop->CountElements();
-		}
-		else
-		{
-			return 0;
-		}
-	}
-	bool Empty() const
-	{
-		return pTop == nullptr;
-	}
-	Iterator begin()
-	{
-		return{ pTop };
-	}
-	Iterator end()
-	{
-		return{};
-	}
-	ConstIterator begin() const
-	{
-		return{ pTop };
-	}
-	ConstIterator end() const
-	{
-		return{};
-	}
-private:
-	Element* pTop = nullptr;
+	//void Print ( );
+
+	void Push ( int val );
+	int Pop ( );  //not done...
+	int Size ( ) const;
+	bool Empty ( ) const;
 };
