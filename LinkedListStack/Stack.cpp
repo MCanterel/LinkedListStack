306c0ca5664ec8601af22f69eb7c4846304ff46d
MCanterel <nelsnelsnels@gmail.com>
2017-10-21 16:35:07 -0400

    Test 1-4 passed . Mem leaks...

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,77 @@
 #include "Stack.h"
+
+//void Stack::Print ( )
+//{
+//	Element *p = head;
+//	if ( p == nullptr ) {
+//		std::cout << "The stack is empty." << std::endl;
+//	}
+//	std::cout << "Stack contents: " << std::endl;
+//	while ( p != nullptr ) {
+//		std::cout << p->val << ", ";
+//		p = p->next;
+//	}
+//}
+
+void Stack::Push ( int val )
+{
+	Element *temp = new Element ( val );
+
+	if ( head == nullptr ) {
+		head = temp;
+		tail = temp;
+	}
+	else {
+		tail->next = temp;
+		tail = temp;
+	}
+}
+
+int Stack::Pop ( )
+{
+	int val = -1;
+	if ( head == nullptr ) {
+		std::cout << "Nothing in the stack." << std::endl;
+	}
+	else if ( tail == head && head != nullptr ) {
+		val = head->val;
+		delete head;
+		tail = nullptr;
+		head = nullptr;
+	}
+	else
+	{
+		Element *temp = head;
+		Element *tempPrev = head;
+		while ( temp != tail ) {
+			tempPrev = temp;
+			temp = temp->next;
+		}
+		val = temp->val;
+		delete temp;
+		tail = tempPrev;
+		tempPrev->next = nullptr;
+	}
+	return val;
+}
+
+int Stack::Size ( ) const
+{
+	if ( head == nullptr ) {
+		return 0;
+	}
+	else {
+		int count = 1;
+		Element *temp = head;
+		while ( temp->next != nullptr ) {
+			temp = temp->next;
+			count++;
+		}
+		return count;
+	}
+}
+
+bool Stack::Empty ( ) const
+{
+	return ( head == nullptr && tail == nullptr );
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -1,10 +1,45 @@
 #pragma once
+#include <iostream>
 
-class Stack
-{
+class Stack {
+private:
+	class Element {
+	public:
+		int val;
+		Element *next;
+		Element ( ) : next ( nullptr ) {};
+		Element ( int val ) :val ( val ), next ( nullptr ) {};
+		/*~Element ( ) {
+			delete next;
+			next = nullptr;
+		}*/
+	};
+private:
+	Element *head;
+	Element *tail;
 public:
-	void Push( int val );
-	int Pop();
-	int Size() const;
-	bool Empty() const;
+	Stack ( ) {
+		head = nullptr;
+		tail = nullptr;
+	}
+	Stack ( const Stack& source ) {
+		head = source.head;
+		tail = source.tail;
+	}
+	/*Stack& operator=( const Stack& source ) {
+
+	}*/
+
+	~Stack ( ) {
+		//delete head;
+		//delete tail;
+		//head = nullptr;
+		//tail = nullptr;
+	}
+	//void Print ( );
+
+	void Push ( int val );
+	int Pop ( );  //not done...
+	int Size ( ) const;
+	bool Empty ( ) const;
 };
